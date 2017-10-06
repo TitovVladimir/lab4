78ba4d137b1b7e52e80649c79ad5d2918274732f
TitovVladimir <chazzii1337@gmail.com>
2017-10-06 19:49:55 +0300

    make gulp_all

--- lw3/gulpfile.js
+++ lw3/gulpfile.js
@@ -1,15 +1,34 @@
 var gulp = require('gulp'),
   uglify = require('gulp-uglify');
   uglifycss = require('gulp-cssmin');
+  copy = require('gulp-contrib-copy'); 
   
 gulp.task('minifyjs', function () {
   gulp.src('js/registration.js')
   .pipe(uglify())
-  .pipe(gulp.dest('build'))
+  .pipe(gulp.dest('build/js'))
 });
   
 gulp.task('minifycss', function () {
   gulp.src('css/style.css')
   .pipe(uglifycss())
-  .pipe(gulp.dest('build'))
+  .pipe(gulp.dest('build/css'))
+});
+
+gulp.task('copyhtml', function() {
+  gulp.src('index.html')
+  .pipe(copy())
+  .pipe(gulp.dest('build'));
+});
+
+gulp.task('gulpall', function () {
+  gulp.src('js/registration.js')
+  .pipe(uglify())
+  .pipe(gulp.dest('build/js'))
+  gulp.src('css/style.css')
+  .pipe(uglifycss())
+  .pipe(gulp.dest('build/css'))
+  gulp.src('index.html')
+  .pipe(copy())
+  .pipe(gulp.dest('build'));
 });
