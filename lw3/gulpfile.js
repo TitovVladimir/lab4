37e329f0cbe5e89b567ee015b53e965009c5565b
TitovVladimir <31964744+TitovVladimir@users.noreply.github.com>
2017-10-06 19:11:40 +0300

    Update gulpfile.js

--- lw3/gulpfile.js
+++ lw3/gulpfile.js
@@ -1,15 +1,15 @@
 var gulp = require('gulp'),
-    uglify = require('gulp-uglify');
-	uglifycss = require('gulp-cssmin');
+  uglify = require('gulp-uglify');
+  uglifycss = require('gulp-cssmin');
   
 gulp.task('minifyjs', function () {
   gulp.src('js/registration.js')
-      .pipe(uglify())
-      .pipe(gulp.dest('build'))
+  .pipe(uglify())
+  .pipe(gulp.dest('build'))
 });
   
 gulp.task('minifycss', function () {
   gulp.src('css/style.css')
-      .pipe(uglifycss())
-      .pipe(gulp.dest('build'))
+  .pipe(uglifycss())
+  .pipe(gulp.dest('build'))
 });
