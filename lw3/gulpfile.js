bd04e8691e5677ffb3780e5aca299408d03c8500
TitovVladimir <chazzii1337@gmail.com>
2017-10-06 20:12:02 +0300

    gulp pref ready

--- lw3/gulpfile.js
+++ lw3/gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
   uglify = require('gulp-uglify');
   uglifycss = require('gulp-cssmin');
   copy = require('gulp-contrib-copy'); 
+  rename = require("gulp-rename");
   
 gulp.task('minifyjs', function () {
   gulp.src('js/registration.js')
@@ -24,9 +25,19 @@ gulp.task('copyhtml', function() {
 gulp.task('gulpall', function () {
   gulp.src('js/registration.js')
   .pipe(uglify())
+  .pipe(rename({
+    basename: "registration",
+    suffix: ".min",
+    extname: ".js"
+  }))
   .pipe(gulp.dest('build/js'))
   gulp.src('css/style.css')
   .pipe(uglifycss())
+  .pipe(rename({
+    basename: "style",
+    suffix: ".min",
+    extname: ".css"
+  }))
   .pipe(gulp.dest('build/css'))
   gulp.src('index.html')
   .pipe(copy())
