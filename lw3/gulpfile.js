180774eec22422c2b58c5fc1aa8c77b0112f7995
TitovVladimir <chazzii1337@gmail.com>
2017-10-06 20:39:59 +0300

    ready lab4

--- lw3/gulpfile.js
+++ lw3/gulpfile.js
@@ -3,6 +3,14 @@ var gulp = require('gulp'),
   uglifycss = require('gulp-cssmin');
   copy = require('gulp-contrib-copy'); 
   rename = require("gulp-rename");
+  browserSync = require('browser-sync');
+  reload = browserSync.reload;
+  paths = {
+    html:['index.html'],
+    css:['css/style.css'],
+    script:['js/registration.js']
+  };
+  
   
 gulp.task('minifyjs', function () {
   gulp.src('js/registration.js')
@@ -43,3 +51,37 @@ gulp.task('gulpall', function () {
   .pipe(copy())
   .pipe(gulp.dest('build'));
 });
+
+gulp.task('html', function(){
+  gulp.src(paths.html)
+  .pipe(reload({stream:true}));
+});
+
+gulp.task('css', function(){
+  gulp.src(paths.css)
+  .pipe(reload({stream:true}));
+});
+
+gulp.task('js', function(){
+  gulp.src(paths.script)
+  .pipe(reload({stream:true}));
+});
+
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: "./"
+    },
+    port: 8080,
+    open: true,
+    notify: false
+  });
+});
+
+gulp.task('watcher',function(){
+  gulp.watch(paths.css, ['css']);
+  gulp.watch(paths.script, ['js']);
+  gulp.watch(paths.html, ['html']);
+});
+
+gulp.task('default', ['watcher', 'browserSync']);
