21865de42b6a88cf1491542863c3c49df2d8d94d
TitovVladimir <chazzii1337@gmail.com>
2017-10-06 19:10:28 +0300

    minify create

---
+++ lw3/gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify');
+	uglifycss = require('gulp-cssmin');
+  
+gulp.task('minifyjs', function () {
+  gulp.src('js/registration.js')
+      .pipe(uglify())
+      .pipe(gulp.dest('build'))
+});
+  
+gulp.task('minifycss', function () {
+  gulp.src('css/style.css')
+      .pipe(uglifycss())
+      .pipe(gulp.dest('build'))
+});
